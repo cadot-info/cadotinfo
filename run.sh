9cb6b942bc7d27d22a2ceb00808108bc10d4fec2
root <michael@cadot.info>
2021-03-05 08:33:49 +0100

    fix(gitignore): vscode remove and scripts

--- run.sh
+++
@@ -1,8 +0,0 @@
-result=${PWD##*/}
-res=${PWD}
-docker kill $result
-docker rm $result
-docker run -d -p 8000:8000  \
-    --name $result \
-    -v $res:/app \
-cadotinfo/docker-php7.4-apache-yarn-composer-symfony5
